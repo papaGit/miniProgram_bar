f072fa72894b4c142b9f6562a3960c46bcc68b42
papa <39357243+papaGit@users.noreply.github.com>
2018-05-25 11:06:11 +0800

    Add files via upload

---
+++ testDemo/components/headbar/headbar.js
@@ -0,0 +1,21 @@
+// components/headbar/headbar.js
+Component({
+
+  /** 
+   * 子组件的接收参数的列表 
+   */
+  properties: {
+    navigationBarTitleText: {// 标题名字
+      type: String,
+      value: '标题名字'
+    },
+
+  },
+  /** 
+   * 组件的初始数据 
+   */
+  data: {
+   
+  },
+
+})
---
+++ testDemo/pages/index/index.js
@@ -0,0 +1,10 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    navigationBarTitleText: "父组件的名字",//导航栏名称，传给头部子组件
+  },
+
+})
